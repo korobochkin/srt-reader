0ff497ed0e8200bf9d71e062e13e63b6fad75a51
Nicolas Korobochkin <477498+korobochkin@users.noreply.github.com>
2025-12-20 23:37:31 +0400

    Updated T_NUMBER regex

--- grammar/srt.pp
+++ grammar/srt.pp
@@ -8,7 +8,7 @@
 // Tokens in order of specificity
 %token  T_TIMECODE      \d{2}:\d{2}:\d{2},\d{3}
 %token  T_ARROW         \h*-->\h*
-%token  T_NUMBER        \d+(?=\r?\n)
+%token  T_NUMBER        (?<=^)\d+(?=\r?\n)
 %token  T_BLANK         \r?\n\r?\n
 %token  T_NEWLINE       \r?\n
 %token  T_TEXT          (?-s).+
